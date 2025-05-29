baee676b06e0b7686a9b57a4fbda91624d448a4c
Matt Turner <matt@back9solutions.com>
2025-05-29 13:13:31 +0100

    add ios implementation

--- ios/Sources/AppAttestPlugin/AppAttestPlugin.swift
+++ ios/Sources/AppAttestPlugin/AppAttestPlugin.swift
@@ -1,23 +1,27 @@
-import Foundation
 import Capacitor
+import DeviceCheck
 
-/**
- * Please read the Capacitor iOS Plugin Development Guide
- * here: https://capacitorjs.com/docs/plugins/ios
- */
 @objc(AppAttestPlugin)
-public class AppAttestPlugin: CAPPlugin, CAPBridgedPlugin {
-    public let identifier = "AppAttestPlugin"
-    public let jsName = "AppAttest"
-    public let pluginMethods: [CAPPluginMethod] = [
-        CAPPluginMethod(name: "echo", returnType: CAPPluginReturnPromise)
-    ]
-    private let implementation = AppAttest()
+public class AppAttestPlugin: CAPPlugin {
 
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
+    @objc func generateKey(_ call: CAPPluginCall) {
+        let keyID = UUID().uuidString
+
+        if #available(iOS 14.0, *) {
+            DCAppAttestService.shared.generateKey { attestationKeyID, error in
+                if let error = error {
+                    call.reject("Failed to generate key: \(error.localizedDescription)")
+                    return
+                }
+
+                if let keyID = attestationKeyID {
+                    call.resolve(["keyId": keyID])
+                } else {
+                    call.reject("Key ID is nil")
+                }
+            }
+        } else {
+            call.reject("App Attest requires iOS 14.0 or newer")
+        }
     }
 }
