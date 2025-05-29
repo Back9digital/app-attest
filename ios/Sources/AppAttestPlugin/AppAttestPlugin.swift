d30853ab9735879f7c067748cf9cd74f4e9c1d99
Matt Turner <matt@back9solutions.com>
2025-05-29 15:34:10 +0100

    Update AppAttestPlugin.swift

--- ios/Sources/AppAttestPlugin/AppAttestPlugin.swift
+++ ios/Sources/AppAttestPlugin/AppAttestPlugin.swift
@@ -1,8 +1,8 @@
 import Capacitor
 import DeviceCheck
 
-@objc(AppAttestPlugin)
-public class AppAttestPlugin: CAPPlugin {
+@objc(AppAttest)
+public class AppAttest: CAPPlugin {
 
     @objc func generateKey(_ call: CAPPluginCall) {
         let keyID = UUID().uuidString
@@ -22,6 +22,33 @@ public class AppAttestPlugin: CAPPlugin {
             }
         } else {
             call.reject("App Attest requires iOS 14.0 or newer")
+        }
+    }
+
+    @objc func generateAssertion(_ call: CAPPluginCall) {
+        guard let keyID = call.getString("keyID"),
+              let clientDataHashBase64 = call.getString("clientDataHash") else {
+            call.reject("Missing 'keyID' or 'clientDataHash' parameters")
+            return
+        }
+        
+        guard let clientDataHash = Data(base64Encoded: clientDataHashBase64) else {
+            call.reject("Invalid base64 'clientDataHash'")
+            return
+        }
+        
+        appAttestService.generateAssertion(forKeyId: keyID, clientDataHash: clientDataHash) { assertion, error in
+            if let error = error {
+                call.reject("Failed to generate assertion: \(error.localizedDescription)")
+                return
+            }
+            guard let assertion = assertion else {
+                call.reject("No assertion returned")
+                return
+            }
+            call.resolve([
+                "assertion": assertion.base64EncodedString()
+            ])
         }
     }
 }
